c8bcbaece5c701d0b4ce2844230bb04d0a935b2b
SANDEEP <99253463+sandeep092002@users.noreply.github.com>
2022-05-06 22:53:57 +0530

    Create emp_dict.py

---
+++ emp_dict.py
@@ -0,0 +1,6 @@
+c={1:['siva','manager',20000,'kakinada'],2:['suresh','manager',14000,'amalapuram'],3:['surya','manager',18000,'kakinada'],4:['ravi','manager',10000,'kakinada'],5:['ramya','manager',5000,'kakinada']}
+x = int(input('The emp no:'))
+print('Name of the emp:',c[x][0])
+print('Emp design:',c[x][1])
+print('Emp sal:',c[x][2])
+print('Emp city:',c[x][3])
