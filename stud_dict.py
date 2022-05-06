2b67485693797e3d91888136054a12595b4127b9
SANDEEP <99253463+sandeep092002@users.noreply.github.com>
2022-05-06 22:51:46 +0530

    Create stud_dict.py

---
+++ stud_dict.py
@@ -0,0 +1,6 @@
+c={1:['swetha','female','mpcs','pass'],2:['dhoni','male','mpcs','pass'],3:['sahiti','female','mecs','fail'],4:['sai','male','mec','fail'],5:['ramya','female','mccs','pass']}
+x = int(input('the student no:'))
+print('name of student:',c[x][0])
+print('gender:',c[x][1])
+print('group:',c[x][2])
+print('result:',c[x][3])
